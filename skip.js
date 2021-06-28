de3c55854da219787f95dba42ca357596bab4c1c
Gabriel A <kore.dv7@gmail.com>
2021-06-28 04:01:30 -0300

    skip script

---
+++ skip.js
@@ -0,0 +1,23 @@
+(() => {
+  const defined = (v) => v !== null && v !== undefined;
+
+  const timeout = setInterval(() => {
+    // First try skip click
+    const skipAdBtn = document.querySelector("button.ytp-ad-skip-button");
+
+    if (skipAdBtn) skipAdBtn.click();
+
+    // Force skip.
+    const ad = [...document.querySelectorAll(".ad-showing")][0];
+    if (defined(ad)) {
+      const video = document.querySelector("video");
+      if (defined(video)) {
+        video.currentTime = video.duration;
+      }
+    }
+  }, 300);
+
+  return function () {
+    clearTimeout(timeout);
+  };
+})();
