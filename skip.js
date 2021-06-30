b06dec91045fc5a80e70fce6d86f463113ce939d
Gabriel A <kore.dv7@gmail.com>
2021-06-30 18:07:43 -0300

    🛠 refactoring & performance

--- skip.js
+++ skip.js
@@ -1,18 +1,39 @@
+const $ = (selector) =>
+  typeof selector === "string" ? document.querySelector(selector) : selector;
+
+const $click = (selector) =>
+  typeof selector === "string"
+    ? document.querySelector(selector).click()
+    : selector.click();
+
 (() => {
   const timeout = setInterval(() => {
     // First try skip click
-    const skipAdBtn = document.querySelector("button.ytp-ad-skip-button");
-    const AdBanner = document.querySelector(".ytp-ad-image-overlay");
+    const skipAdBtn = $("button.ytp-ad-skip-button");
+    const skipAdBtn = $("button.ytp-ad-skip-button"); // Ad Btn
+    const AdBanner = $(".ytp-ad-image-overlay"); // Remove ad banner
+    const AdBannerTwo = $("button.ytp-ad-overlay-close-button");
 
     if (AdBanner) AdBanner.style.display = "none";
-    if (skipAdBtn) skipAdBtn.click();
+    if (skipAdBtn) $click(skipAdBtn);
+    if (AdBannerTwo) $click(AdBannerTwo);
 
-    // Force skip.
-    const ad = [...document.querySelectorAll(".ad-showing")][0];
-    const video = document.querySelector("video");
+    // Force skip ad if exists.
+    const videoAd = $(".ad-showing video");
 
-    if (ad && video) video.currentTime = 10000;
-  }, 50);
+    if (videoAd) {
+      if (videoAd.currentTime === videoAd.duration) {
+        let skipAd = $("button.ytp-ad-skip-button"); // Click on "skip"
+        $click(skipAd);
+      }
+
+      videoAd.currentTime = videoAd.duration; // Skip to end
+
+      let skipAd = $("button.ytp-ad-skip-button"); // Click on "skip"
+
+      if (skipAd) $click(skipAd);
+    }
+  }, 100);
 
   return function () {
     clearTimeout(timeout);
