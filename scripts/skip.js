cc96e90268703a099fbebd011bd6793b1b0f5ce7
Gabriel A <kore.dv7@gmail.com>
2021-07-11 06:42:03 -0300

    use MutatioObserver to observe ad.

--- scripts/skip.js
+++ scripts/skip.js
@@ -15,30 +15,50 @@ const $css = (styles) => {
 };
 
 window.onload = () => {
-  let styles = ".ytp-ad-image-overlay { display: 'none'!important } ";
+  let styles = ".ytp-ad-image-overlay { display: none!important } ";
   styles +=
-    ".ytp-ad-overlay-container, #player-ads { display: 'none'!important } ";
+    ".ytp-ad-overlay-container, #player-ads { display: none!important } ";
+  styles += ".ytp-ad-message-overlay { display: none!important } ";
+  styles += ".video-ads { display: none!important } ";
+  styles += ".ytmusic-popup-container { display: none!important } ";
 
   $css(styles);
+
+  injectObserver();
+};
+
+const injectObserver = () => {
+  const player = $(".ytd-player");
+
+  if (player) {
+    const observer = new MutationObserver((mutations) => {
+      for (let mutation of mutations) {
+        if (mutation.target.matches("div.video-ads.ytp-ad-module")) {
+          const element = document.querySelector(".ad-showing video");
+          $click("button.ytp-ad-skip-button"); // Click on "skip"
+
+          if (element) {
+            if (isFinite(element.duration)) {
+              element.currentTime = element.duration;
+              $click("button.ytp-ad-skip-button"); // Click on "skip"
+            }
+          }
+        }
+      }
+    });
+
+    observer.observe(player, {
+      childList: true,
+      subtree: true,
+    });
+  }
 };
 
 (() => {
-  const timeout = setInterval(() => {
+  const timeout = setTimeout(() => {
     $click("button.ytp-ad-overlay-close-button"); // Try close ad banner
     $click("button.ytp-ad-skip-button");
-
-    // Force skip ad if exists.
-    const videoAd = $(".ad-showing video");
-
-    if (videoAd) {
-      // Prevent non-finite value.
-      if (isFinite(video.duration)) {
-        videoAd.currentTime = videoAd.duration; // Skip to end
-      }
-
-      $click("button.ytp-ad-skip-button"); // Click on "skip"
-    }
-  }, 100);
+  }, 200);
 
   return function () {
     clearTimeout(timeout);
