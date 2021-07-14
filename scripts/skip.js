b267e1aa1010bed8b4185328689bc48e3acbd2c2
Gabriel A <kore.dv7@gmail.com>
2021-07-14 03:02:57 -0300

    Improve performance skip ads

--- scripts/skip.js
+++ scripts/skip.js
@@ -14,91 +14,44 @@ const $css = (styles) => {
   document.head.appendChild(css);
 };
 
-window.onload = async () => {
-  let styles = ".ytp-ad-image-overlay { display: none!important } ";
-  styles +=
-    ".ytp-ad-overlay-container, #player-ads { display: none!important } ";
-  styles += ".ytp-ad-message-overlay { display: none!important } ";
-  styles += ".video-ads { display: none!important } ";
-  styles += ".ytmusic-popup-container { display: none!important } ";
-
-  $css(styles);
-
-  clearFirstAd();
-
-  const player = await each("#movie_player");
-
-  console.log(player);
-
-  mountObserver(player);
-};
-
-const clearFirstAd = () => {
-  let attempts = 0;
-
-  while (attempts < 3) {
-    attempts++;
-
-    skipAd();
-  }
-};
-
 const each = async (selector) => {
   return new Promise((resolve) => {
-    let timeout = setTimeout(() => {
+    let timeout = setInterval(() => {
       const element = document.querySelector(selector);
 
       if (element) {
         resolve(element);
-        return clearTimeout(timeout);
+        return clearInterval(timeout);
       }
-    }, 50);
+    }, 10);
   });
 };
 
-const skipAd = () => {
-  $click("button.ytp-ad-overlay-close-button"); // Try close ad banner
+window.onload = async () => {
+  let styles = `
+    .ytp-ad-image-overlay { display: none!important } 
+    .ytp-ad-overlay-container, #player-ads { display: none!important } 
+    .ytp-ad-message-overlay { display: none!important } 
+    .video-ads { display: none!important } 
+    .ytmusic-popup-container { display: none!important } 
+  `;
 
-  const element = document.querySelector(".ad-showing video");
+  $css(styles);
+};
 
-  if (element) {
+(() => {
+  let interval = setInterval(() => {
+    const ad = $(".ad-showing video");
+
+    if (!ad) return;
+
+    //Skip
+    isFinite(ad.duration) && (ad.currentTime = ad.duration);
+
     $click("button.ytp-ad-skip-button"); // Click on "skip"
-    if (isFinite(element.duration)) {
-      element.currentTime = element.duration;
-      $click("button.ytp-ad-skip-button"); // Click on "skip"
-    }
-  }
-};
+  }, 10);
 
-const mountObserver = (player) => {
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.attributeName == "class") {
-        var adShowing = mutation.target.classList.contains(".ad-showing");
-
-        if (adShowing) {
-          console.log("Cleared by observer");
-
-          const element = document.querySelector(".ad-showing video");
-
-          if (isFinite(element.duration)) {
-            element.currentTime = element.duration;
-            $click("button.ytp-ad-skip-button"); // Click on "skip"
-          }
-        }
-      }
-    });
-  });
-
-  observer.observe(player, {
-    attributeOldValue: true,
-    attributes: true,
-  });
-};
-
-var attempts = 0;
-let attepmttry = setTimeout(() => {
-  skipAd();
-  attempts++;
-  if (attempts > 10) clearTimeout(attepmttry);
-}, 50);
+  return function () {
+    clearInterval(interval);
+  };
+})();
